9ee8bd713fecbac64a2971bd646f315994b028d7
MeharinChoudhury23040 <it23040@mbstu.ac.bd>
2025-02-17 13:03:56 +0600

    Add files via upload

---
+++ FactorialBigInteger.java
@@ -0,0 +1,22 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class FactorialBigInteger {
+    // Method to compute factorial using BigInteger
+    public static BigInteger factorial(int n) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        System.out.println(num + "! is \n" + factorial(num));
+    }
+}
