b2b3cb70d0d487d048bc5be78909909021a19a43
MeharinChoudhury23040 <it23040@mbstu.ac.bd>
2025-02-17 13:08:29 +0600

    Update FactorialBigInteger.java

--- FactorialBigInteger.java
+++ FactorialBigInteger.java
@@ -1,22 +1,26 @@
 import java.math.BigInteger;
 import java.util.Scanner;
 
-public class FactorialBigInteger {
+public class FactorialBigInteger
+    {
     // Method to compute factorial using BigInteger
-    public static BigInteger factorial(int n) {
-        BigInteger result = BigInteger.ONE;
-        for (int i = 1; i <= n; i++) {
-            result = result.multiply(BigInteger.valueOf(i));
+    public static BigInteger factorial(int n) 
+        {
+        BigInteger result=BigInteger.ONE;
+        for (int i=1;i<=n;i++) 
+        {
+            result=result.multiply(BigInteger.valueOf(i));
         }
         return result;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int num = scanner.nextInt();
+        int num=scanner.nextInt();
         scanner.close();
 
-        System.out.println(num + "! is \n" + factorial(num));
+        System.out.println(num+"! is \n"+factorial(num));
     }
 }
